b2ede9e177c8fbea139d3b4b172b23b9cd701d01
toshi-miura <toshihiko.miura@gmail.com>
2019-10-08 00:08:58 +0900

    Fix typos and fix verbose logs

--- providers/github.go
+++ providers/github.go
@@ -181,21 +181,18 @@ func (p *GitHubProvider) hasOrgAndTeam(accessToken string) (bool, error) {
 			// 1. When paging is required (Example: When the data size is 100 and the page size is 99 or less)
 			// 2. When it exceeds the paging frame (Example: When there is only 10 records but the second page is called with a page size of 100)
 
-			// link herder at not last page
+			// link header at not last page
 			// <https://api.github.com/user/teams?page=1&per_page=100>; rel="prev", <https://api.github.com/user/teams?page=1&per_page=100>; rel="last", <https://api.github.com/user/teams?page=1&per_page=100>; rel="first"
-			// link herder at last page (doesn't exist last info)
+			// link header at last page (doesn't exist last info)
 			// <https://api.github.com/user/teams?page=3&per_page=10>; rel="prev", <https://api.github.com/user/teams?page=1&per_page=10>; rel="first"
 
-			logger.Printf("1:endpoint :[%s]",endpoint.String())
-			logger.Printf("2:link header:[%s]",resp.Header.Get("Link"))		
+
 			link := resp.Header.Get("Link")
 			rep1 := regexp.MustCompile(`(?s).*\<https://api.github.com/user/teams\?page=(.)&per_page=[0-9]+\>; rel="last".*`)
 			i,converr:= strconv.Atoi(rep1.ReplaceAllString(link, "$1"))
 			if converr != nil{
-				logger.Printf("3:doesn't exist last page info. ")
 			}else{
 				last = i
-				logger.Printf("3:last page index  :[%d]", last)
 			}
 		}
 
@@ -219,11 +216,9 @@ func (p *GitHubProvider) hasOrgAndTeam(accessToken string) (bool, error) {
 		teams = append(teams, tp...)
 		
 		if pn == last {			
-			logger.Printf("last page break  now:[%d]  last[%d]",  pn,last)
 			break
 		}
 		if last == 0 {			
-			logger.Printf("doesn't exist last page info. Now the last page.")
 			break
 		}
 
